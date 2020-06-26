7980068eaed9f30184e819b49881652b92332d5f
ziruihao <ziray.hao.22@dartmouth.edu>
2020-06-26 18:26:13 -0400

    build

--- index.js
+++ index.js
@@ -9,140 +9,165 @@ var _react = _interopRequireDefault(require("react"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var Box = function Box(props) {
-  var style = {};
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  if (props.style) {
-    style = _objectSpread({}, props.style);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var Box = /*#__PURE__*/function (_React$Component) {
+  _inherits(Box, _React$Component);
+
+  var _super = _createSuper(Box);
+
+  function Box() {
+    _classCallCheck(this, Box);
+
+    return _super.apply(this, arguments);
   }
 
-  if (props.row) {
-    style.flexDirection = 'row';
-  } else if (props.col) {
-    style.flexDirection = 'col';
-  }
+  _createClass(Box, [{
+    key: "render",
+    value: function render() {
+      var style = {};
 
-  if (props.justify) {
-    switch (props.justify) {
-      case 'start':
-        style.justifyContent = 'flex-start';
-        break;
-
-      case 'end':
-        style.justifyContent = 'flex-end';
-        break;
-
-      case 'center':
-        style.justifyContent = 'center';
-        break;
-
-      case 'between':
-        style.justifyContent = 'space-between';
-        break;
-
-      case 'around':
-        style.justifyContent = 'space-around';
-        break;
-    }
-  }
-
-  if (props.align) {
-    switch (props.align) {
-      case 'start':
-        style.alignItems = 'flex-start';
-        break;
-
-      case 'end':
-        style.alignItems = 'flex-end';
-        break;
-
-      case 'center':
-        style.alignItems = 'center';
-        break;
-
-      case 'stretch':
-        style.alignItems = 'stretch';
-        break;
-    }
-  }
-
-  if (props.self) {
-    switch (props.self) {
-      case 'start':
-        style.alignSelf = 'flex-start';
-        break;
-
-      case 'end':
-        style.alignSelf = 'flex-end';
-        break;
-
-      case 'center':
-        style.alignSelf = 'center';
-        break;
-
-      case 'stretch':
-        style.alignSelf = 'stretch';
-        break;
-    }
-  }
-
-  if (props.wrap) {
-    style.flexWrap = 'wrap';
-  }
-
-  if (props.flex) style.flex = props.flex;
-  if (props.width) style.width = props.width;
-  if (props.height) style.height = props.height;
-
-  if (props.pad) {
-    if (Array.isArray(props.pad)) {
-      switch (props.pad.length) {
-        case 1:
-          style.padding = props.pad[0];
-          break;
-
-        case 2:
-          style.paddingVertical = props.pad[0];
-          style.paddingHorizontal = props.pad[1];
-          break;
-
-        case 4:
-          style.paddingTop = props.pad[0];
-          style.paddingBottom = props.pad[1];
-          style.paddingLeft = props.pad[2];
-          style.paddingRight = props.pad[3];
-          break;
+      if (props.style) {
+        style = _objectSpread({}, props.style);
       }
-    } else {
-      style.padding = props.pad;
+
+      if (props.row) {
+        style.flexDirection = 'row';
+      } else if (props.col) {
+        style.flexDirection = 'col';
+      }
+
+      if (props.justify) {
+        switch (props.justify) {
+          case 'start':
+            style.justifyContent = 'flex-start';
+            break;
+
+          case 'end':
+            style.justifyContent = 'flex-end';
+            break;
+
+          case 'center':
+            style.justifyContent = 'center';
+            break;
+
+          case 'between':
+            style.justifyContent = 'space-between';
+            break;
+
+          case 'around':
+            style.justifyContent = 'space-around';
+            break;
+        }
+      }
+
+      if (props.align) {
+        switch (props.align) {
+          case 'start':
+            style.alignItems = 'flex-start';
+            break;
+
+          case 'end':
+            style.alignItems = 'flex-end';
+            break;
+
+          case 'center':
+            style.alignItems = 'center';
+            break;
+
+          case 'stretch':
+            style.alignItems = 'stretch';
+            break;
+        }
+      }
+
+      if (props.self) {
+        switch (props.self) {
+          case 'start':
+            style.alignSelf = 'flex-start';
+            break;
+
+          case 'end':
+            style.alignSelf = 'flex-end';
+            break;
+
+          case 'center':
+            style.alignSelf = 'center';
+            break;
+
+          case 'stretch':
+            style.alignSelf = 'stretch';
+            break;
+        }
+      }
+
+      if (props.wrap) {
+        style.flexWrap = 'wrap';
+      }
+
+      if (props.flex) style.flex = props.flex;
+      if (props.width) style.width = props.width;
+      if (props.height) style.height = props.height;
+
+      if (props.pad) {
+        if (Array.isArray(props.pad)) {
+          switch (props.pad.length) {
+            case 1:
+              style.padding = props.pad[0];
+              break;
+
+            case 2:
+              style.paddingVertical = props.pad[0];
+              style.paddingHorizontal = props.pad[1];
+              break;
+
+            case 4:
+              style.paddingTop = props.pad[0];
+              style.paddingBottom = props.pad[1];
+              style.paddingLeft = props.pad[2];
+              style.paddingRight = props.pad[3];
+              break;
+          }
+        } else {
+          style.padding = props.pad;
+        }
+      }
+
+      return /*#__PURE__*/_react["default"].createElement("div", {
+        id: props.id ? props.id : null,
+        className: props.className ? props.className : null,
+        style: style
+      }, props.children);
     }
-  }
+  }]);
 
-  return /*#__PURE__*/_react["default"].createElement("div", {
-    id: props.id ? props.id : null,
-    className: props.className ? props.className : null,
-    style: style
-  }, props.children);
-};
+  return Box;
+}(_react["default"].Component);
 
-var _default = Box;
-exports["default"] = _default;
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-
-var _box = _interopRequireDefault(require("./box"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var _default = _box["default"];
-exports["default"] = _default;
+exports["default"] = Box;
